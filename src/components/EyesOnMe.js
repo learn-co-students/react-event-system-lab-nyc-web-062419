ca5cea185d7f2cf63b479591d5682c1425154761
thenatural86 <philip.smalls86@gmail.com>
2019-08-26 23:44:03 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React, { PureComponent } from "react"
+
+export default class EyesOnMe extends PureComponent {
+  constructor() {
+    super()
+  }
+
+  handleFocus = () => {
+    console.log("Good!")
+  }
+
+  handleBlur = () => {
+    console.log("Hey! Eyes on me!")
+  }
+
+  render() {
+    return <button onFocus={this.handleFocus} onBlur={this.handleBlur}></button>
+  }
+}
